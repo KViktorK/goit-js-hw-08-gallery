c0de9ba6afc3a5a76ecf42e6e9d7e49d3a97b4d2
KViktorK <72447099+KViktorK@users.noreply.github.com>
2021-04-14 20:14:42 +0200

    Готовое задание

---
+++ src/gallery.js
@@ -0,0 +1,65 @@
+import galleryArr from './gallery-items.js'
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    modal: document.querySelector('.lightbox'),
+    modalOverlay: document.querySelector('.lightbox__overlay'),
+    modalImg: document.querySelector('.lightbox__image'),
+    closeBtn: document.querySelector('[data-action="close-lightbox"]')
+}
+// Создание разметки
+const createListItem = galleryArr.map(({original,preview,description}) => {
+    const liItem = document.createElement('li');
+    liItem.insertAdjacentHTML('afterbegin',
+    `<li class="gallery__item">
+    <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    return liItem
+})
+refs.gallery.append(...createListItem)
+
+// Делегирование 
+refs.gallery.addEventListener('click', e => {
+    if (e.target.localName === 'img') {
+        e.preventDefault(); 
+        refs.modal.classList.add('is-open');
+        refs.modalImg.src = e.target.dataset.source;
+    }
+})
+refs.closeBtn.addEventListener('click', e => {
+    if (e.target.localName === 'button') {
+        refs.modal.classList.remove('is-open');
+        refs.modalImg.src = '';
+    }
+})
+refs.modalOverlay.addEventListener('click', e => {
+    if (e.target.localName !== 'img') {
+        refs.modal.classList.remove('is-open');
+        refs.modalImg.src = '';
+    }
+})
+window.addEventListener('keyup', e => {
+    if(e.key === 'Escape') {
+        refs.modal.classList.remove('is-open')
+    }
+})
+refs.modalOverlay.addEventListener('keyup', e => {
+    if (e.key === 'ArrowRight') {
+        e.setAttribute(src, 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg')
+    }
+})
+window.addEventListener('keyup', e => {
+    if(e.key === 'ArrowLeft') {
+        console.log(e.key)
+    }
+})
